9e6180c7de9a66635f64e4def63cc27e2d614478
hans-and <hans.andersson@gmail.com>
2021-05-14 17:20:15 +0200

    Hupp

--- src/main/java/hasses/magical/helpers/StringHelper.java
+++ src/main/java/hasses/magical/helpers/StringHelper.java
@@ -3,36 +3,37 @@ package hasses.magical.helpers;
 public class StringHelper {
 
 	public static boolean isNullOrEmptyOrBlank(String candidate) {
-		
-		return candidate==null||candidate.trim().isEmpty();
-	}
 
+		return candidate == null || candidate.trim().isEmpty();
+	}
 
 	public static String toCommaString(Object... object) {
-		
+
 		return toObjArrtoCommaString(object);
 	}
-	
-public static String toObjArrtoCommaString(Object[] objects) {
-	    StringBuilder b = new StringBuilder();
-	    
-		for (int i = 0; i < objects.length-1; i++) {
-			b.append(objects[1]+",");
+
+	public static String toObjArrtoCommaString(Object[] objects) {
+		StringBuilder b = new StringBuilder();
+
+		for (int i = 0; i < objects.length - 1; i++) {
+			b.append(objects[1] + ",");
 		}
-		b.append(objects.length-1);
+		b.append(objects.length - 1);
 		return b.toString();
 	}
 
-	
-	public static boolean anyNullOrEmptyOrBlank(String ... candidates) {
+	public static boolean anyNullOrEmptyOrBlank(String... candidates) {
 		for (String candidate : candidates) {
-			if(isNullOrEmptyOrBlank(candidate)){
+			if (isNullOrEmptyOrBlank(candidate)) {
 				return true;
 			}
 		}
 		return false;
 	}
 
-	
-	
+	public static boolean isNullOrEmpty(String candidate) {
+
+		return candidate == null || candidate.isEmpty();
+	}
+
 }
